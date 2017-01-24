60dafb48feb3e570a1b384f06283d444463e683d
MarcWong <brucewayne@pku.edu.cn>
2017-01-24 22:17:39 +0800

    0124 not big progress
    homepage: limited maximum item number
    css editing
    figured out how to do pagination, later show in search.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -100,30 +100,32 @@
 @if (isset($products) > 0)
     <div class="container" style="width:100%;">
         <div class="row" style="width:100%;margin-top:58px;padding-bottom:80px;">
-            @foreach ($products as $product)
-                    <div class="col-sm-12 col-md-6 col-lg-4">
-                        <div class="thumbnail" >
-                            <!--<img src="images/{{$product->image_file}}" class="img-responsive">-->
+            <!--{{$i=0}}-->
+            @for ($i = 1; $i < count($products);$i++)
+            <!--foreach (products as product)-->
+                @if ($i<=12)
+                    <!--{{$product=$products[$i]}}-->
+                    <div class="col-xs-6 col-sm-6 col-md-4 col-lg-3">
+                        <div class="thumbnail">
                             <div class="demo-image" data-image="/api/product/images/{{$product->image_file}}" data-title="{{$product->title}}" data-caption="{{$product->description}}"><img src="/api/product/images/{{$product->image_file}}" class="img-responsive"></div>
-                            <div class="caption">
+                            <div class="caption" style="padding-top:0px;">
                                 <div class="row">
                                     <div class="col-lg-12 col-md-12 col-sm-12">
                                         <div style="color:#9aa4af; overflow:hidden; height:35px;">
                                             <p style="margin:0 0 0 0px;">{{$product->description}}</p>
                                         </div>
-                                        <div class="col-lg-8 col-md-8 col-sm-7 col-xs-6" style="text-align:left;padding-left: 0px;">
-                                            <div class="col-lg-1 col-md-1 col-sm-1" style="padding-left:5px;">
+                                        <div class="col-lg-9 col-md-9 col-sm-8 col-xs-8" style="text-align:left;padding-left: 0px;">
+                                            <div class="col-lg-1 col-md-1 col-sm-1 col-xs-1" style="padding-left:5px;">
                                             <img style="width:20px;" src="/img/icons/svg/clocks.svg"/>
                                             </div>
-                                            <div class="col-lg-9 col-md-9 col-sm-9">
-                                            <p style="color:#bdc3c7; font-size:15px; margin-top:5px;">{{substr($product->created_at,0,10)}}<p>
+                                            <div class="col-lg-8 col-md-8 col-sm-8 col-xs-8">
+                                            <p style="color:#bdc3c7; font-size:15px; margin-top:2px;">{{substr($product->created_at,0,10)}}<p>
                                             </div>
                                         </div>
                                         
-                                        <div class="col-lg-4 col-md-4 col-sm-5 col-xs-6" style="text-align:left;">
+                                        <div class="col-lg-3 col-md-3 col-sm-4 col-xs-4" style="text-align:left;">
                                             <p style="color:#f44336; margin-top:0px;">￥{{$product->price}}</p>
                                         </div>
-                                        <!--<h5>{{$product->title}}</h5>-->
                                         <!--<p>{{$product->image_file}}</p>-->
                                         <!--<p>created by <a href="#" class="bambu-color1">{{$product->user_name}}</a></p>-->
                                     </div>
@@ -136,15 +138,12 @@
                             </div>
                         </div>
                     </div>
-            @endforeach
-            <!--pagination
-            <ul class="col-sm-12 col-md-12 col-lg-12 pagination">
-            @for ($i = 1; $i < count($products)/3;$i++)
-                @if($i==1)
-                <li class="bambu-color1"><a>{{$i}}</a></li>
-                @else
-                <li><a>{{$i}}</a></li>
                 @endif
+            @endfor
+            <!--pagination not used in homepage
+            <ul class="col-sm-3 col-md-3 col-lg-2 pagination">
+            @for ($i = 1; $i < count($products)/3;$i++)
+                <li id="{{$i}}" class="bambu-color1" onclick="turnpage({{$i}})"><a>{{$i}}</a></li>
             @endfor
             </ul>
             -->
@@ -165,6 +164,11 @@
 
 </body>
     <script type="text/javascript">
+        function turnpage(id){
+
+            window.location.href="/";
+        }
+
         function sb(){
             s = document.getElementById('inpu1').value;
             if(s){
