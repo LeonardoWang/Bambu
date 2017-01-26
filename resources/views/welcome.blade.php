895066815401c9e518b5d59b95e0d7a2997cbd44
marcWong <yao.staufenberg@gmail.com>
2017-01-26 19:26:23 +0800

    navbar modified

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -59,11 +59,15 @@
                     <a href="/api/product/myProduct">my items</a>
                 </li>
                 <li><a href="#aboutUs">about us</a></li>
-                <li><div class="navbar-form navbar-right">
+                <li><div class="navbar-form navbar-right col-xs-2" style="margin-left:0px;padding-left:21px;">
                     <div class="form-group">
-                    <input type="text" id="inpu1" name="keyword" class="form-control" placeholder="Search"/>
-                    </div>
-                    <button onclick="sb()" class="btn btn-primary bambu-color1" style="background-color:#f44336">search</button>
+		    <div class="input-group">
+                    <input type="text" id="inpu1" class="form-control" placeholder="Search"/>
+                    <span class="input-group-btn">
+    			<button onclick="sb()" class="btn"><span class="fui-search"></span></button>
+  	   	    </span>
+		    </div>
+		    </div>
                     </div>
                 </li>
                </ul>
