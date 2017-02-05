53d0ca0c4a94aebf8e52258bbdb861a5e50a9e23
Leonardo <leonardo@LeonardodeMacBook-Pro.local>
2017-02-05 19:51:39 +0800

    tmp

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -56,8 +56,9 @@ Route::group(['prefix' => 'api','middleware' => 'auth'], function () {
 	Route::get('items/{id}/delete', 'Api\ItemsController@destroy');
 	Route::get('items/{id}/images', 'Api\ItemsController@images');
 	Route::get('items/search/{keyword}', 'Api\ItemsController@search');
-	Route::get('trade_requests/{id}','Api\TradeRequestsController@doRequest');
 	Route::get('trade_requests/my','Api\TradeRequestsController@myRequest');
+	Route::get('trade_requests/{id}','Api\TradeRequestsController@doRequest');
+	
 	Route::post('trade_request_making','Api\TradeRequestsController@postRequest');
 	//Route::resource('trade_requests', 'Api\TradeRequestsController', ['only' => ['index', 'store', 'show']]);
 	
