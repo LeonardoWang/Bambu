1597542105ffd5a25203eff6fa782c19a61b50bd
MarcWong <brucewayne@pku.edu.cn>
2017-02-25 13:31:18 +0800

    myprofile edit

--- resources/views/myprofile.blade.php
+++ resources/views/myprofile.blade.php
@@ -20,8 +20,7 @@ myprofile page
                                             <img style="width:100px;height:100px;margin-bottom:10px;" src="/api/product/images/{{$user_information->user_image}}" class="img-circle"/>
                                         @endif
                                     <div class="form-group">
-                                        <label class="col-md-3 col-sm-3 col-xs-3 control-label" for="name">Profile picture</label>
-                                        <div class="col-md-9 col-sm-9 col-xs-9">
+                                        <div class="col-md-8 col-md-offset-2">
                                             <input id="image" name="image" class="form-control input-sm" type="file">
                                             <!--<button type="button" onclick="uploadProfilePicture()" class="btn btn-xs">upload your profile picture</button>-->
                                         </div>
@@ -29,27 +28,23 @@ myprofile page
                                     <h6>{{$user->name}}'s Profile</h6>
                                     <hr>
                                     <div class="form-group">
-                                        <label class="col-md-3 col-sm-3 col-xs-3 control-label" for="name">Name</label>
-                                        <div class="col-md-9 col-sm-9 col-xs-9">
-                                            <input id="name" name="name" type="text" value="{{$user->name}}" class="form-control input-md" required="">
+                                        <div class="col-md-8 col-md-offset-2">
+                                            <input id="name" name="name" type="text" value="{{$user->name}}" placeholder="Name" class="form-control input-md" required="">
                                         </div>
                                     </div>
                                     <div class="form-group">
-                                        <label class="col-md-3 col-sm-3 col-xs-3 control-label" for="tel">Phone</label>
-                                        <div class="col-md-9 col-sm-9 col-xs-9">
-                                            <input id="tel" name="tel" type="text" value="{{$user->tel}}" class="form-control input-md" readOnly="true" style="border: 2px solid #bdc3c7;opacity:1;color:#34495e;background-color:white;">
+                                        <div class="col-md-8 col-md-offset-2">
+                                            <input id="tel" name="tel" type="text" value="{{$user->tel}}" class="form-control input-md" readOnly="true" style="border-width:0px;color:black;">
                                         </div>
                                     </div>
                                     <div class="form-group">
-                                        <label class="col-md-3 col-sm-3 col-xs-3 control-label" for="location">Location</label>
-                                        <div class="col-md-9 col-sm-9 col-xs-9">
-                                            <input id="location" name="location" type="text" value="{{$user_information->location}}" class="form-control input-md">
+                                        <div class="col-md-8 col-md-offset-2">
+                                            <input id="location" name="location" type="text" value="{{$user_information->location}}" placeholder="Location" class="form-control input-md">
                                         </div>
                                     </div>
                                     <div class="form-group">
-                                        <label class="col-md-3 col-sm-3 col-xs-3 control-label" for="sex">Gender</label>
-                                        <div class="col-md-9 col-sm-9 col-xs-9">
-                                            <select id="sex" name="sex" class="form-control input-md" required="required">
+                                        <div class="col-md-8 col-md-offset-2">
+                                            <select id="sex" name="sex" placeholder="Gender" class="form-control input-md" required="required">
                                                 <option value="male" {{($user_information->sex=="male")?"selected=":""}}>Male</option>
                                                 <option value="female" {{($user_information->sex=="female")?"selected=":""}}>Female</option>
                                                 <option value="unknown" {{($user_information->sex=="unknown")?"selected=":""}}>Unknown</option>
